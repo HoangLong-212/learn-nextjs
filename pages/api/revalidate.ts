286bb4dbe498f5fa4f26bb5afe733443c866f671
HoangLong-212 <longth221@gmail.com>
2022-10-04 08:10:47 +0700

    refactor code

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,9 +1,9 @@
 export default async function handler(req: any, res: any) {
   await res.revalidate("/products");
 
-  const pathToRevalidate = `/${
-    req.body?.record?.id || req.body?.old_record?.id
-  }`;
-  await res.revalidate(pathToRevalidate);
+  // const pathToRevalidate = `/${
+  //   req.body?.record?.id || req.body?.old_record?.id
+  // }`;
+  // await res.revalidate(pathToRevalidate);
   return res.json({ revalidated: true });
 }
